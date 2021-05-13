cb2667c5e6c947c09c1ffd69ac74f0f91b23f37c
sunladen <sunladenmail@gmail.com>
2021-05-13 18:37:03 +1000

    grey hairs

--- server.mjs
+++ server.mjs
@@ -4,37 +4,8 @@ import WebSocket from 'ws';
 import http from 'http';
 import fs from 'fs';
 import sqlite3 from 'sqlite3';
+import crypto from 'crypto';
 import encoding from './encoding.mjs';
-
-// init .data directory
-const datapath = './.data';
-if ( ! fs.existsSync( datapath ) ) fs.mkdirSync( datapath );
-
-// init sqlite db
-const dbpath = './.data/sqlite.db';
-const exists = fs.existsSync( dbpath );
-const db = new sqlite3.Database( dbpath );
-
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize( () => {
-
-	if ( ! exists ) {
-
-		db.run( 'CREATE TABLE accounts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)' );
-		console.log( "New table accounts created!" );
-
-	} else {
-
-		console.log( 'Database "accounts" ready to go!' );
-		db.each( "SELECT * from accounts", ( err, row ) => {
-
-			if ( row ) console.log( `record: ${row.name}` );
-
-		} );
-
-	}
-
-} );
 
 
 const CLIENT_UPDATE_INTERVAL = 10000;
@@ -44,8 +15,12 @@ const ws_identified = {};
 const ws_closed = {};
 const requests = {};
 
-
 const blacklisted_names = [ 'undefined', 'null', 'name', 'admin' ];
+
+const datapath = './.data';
+const dbpath = `${datapath}/sqlite.db`;
+
+var wss;
 
 
 requests.ident = ( ws, id ) => {
@@ -141,159 +116,181 @@ function broadcast( origin_ws, message ) {
 
 }
 
+// create .data directory if it doesn't exist
+if ( ! fs.existsSync( datapath ) ) fs.mkdirSync( datapath );
 
-var wss;
+// check if database needs initialising
+const dbinit = fs.existsSync( dbpath );
+const db = new sqlite3.Database( dbpath );
 
+db.serialize( () => {
 
+	if ( ! dbinit ) {
 
-if ( process.mainModule === this ) {
+		db.run( 'CREATE TABLE accounts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)' );
+		console.log( "New table accounts created!" );
 
-	process.on( "SIGINT", () => {
+	} else {
 
-		console.log( "SIGINT received, stopping..." );
-		broadcast( null, [ 'say', null, 'Server shut down' ] );
-		process.exit();
+		console.log( 'Database "accounts" ready to go!' );
+		db.each( "SELECT * from accounts", ( err, row ) => {
 
-	} );
-
-
-	const httpserver = http.createServer( ( req, res ) => {
-
-		if ( "GET" === req.method ) {
-
-			var content = "";
-
-			try {
-
-				var filepath = "." + ( ( "/" === req.url ) ? "/index.html" : req.url );
-
-				content = fs.readFileSync( filepath );
-
-				res.writeHead( 200, {
-					"Content-Type": {
-						html: "text/html",
-						js: "text/javascript",
-						mjs: "text/javascript",
-						css: "text/css",
-						json: "application/json"
-					}[ filepath.split( "." ).pop().toLowerCase() ] || "text/*"
-				} );
-
-			} catch ( err ) {
-
-				res.code = "ENOENT" === err.code ? 404 : 500, res.writeHead( res.code, { "Content-Type": "text/html" } ), content = "<!doctype html><html><head><title>PBBG</title></head><body>" + err.message + "</body></html>";
-
-			}
-
-			return res.end( content, "utf-8" );
-
-		}
-
-	} );
-
-
-	httpserver.listen( process.env.PORT || 7714, () => {
-
-		console.log( `Server started on ${process.env.PORT ? 'port ' + process.env.PORT : 'http://localhost:7714' }` );
-
-	} );
-
-	wss = new WebSocket.Server( { server: httpserver } );
-
-	wss.on( 'connection', ws => {
-
-		ws.id = 'Guest-' + Math.floor( ( 1 + Math.random() ) * 0x10000 ).toString( 16 ).substring( 1 );
-		ws.last_heard = ( new Date() ).getTime();
-
-		ws.on( 'close', () => {
-
-			if ( ws_connection.hasOwnProperty( ws.id ) ) delete ws_connection[ ws.id ];
-			if ( ws_identified.hasOwnProperty( ws.id ) ) delete ws_identified[ ws.id ];
-			ws_closed[ ws.id ] = ws;
+			if ( row ) console.log( `record: ${row.name}` );
 
 		} );
 
-		ws.on( 'pong', () => {
+	}
 
-			if ( ! ws_identified.hasOwnProperty( ws.id ) ) return;
-			ws_identified[ ws.id ].last_heard = ( new Date() ).getTime();
+} );
 
-		} );
 
-		ws.on( 'message', message => {
 
-			var encoded_message = message.buffer.slice( message.byteOffset, message.byteOffset + message.byteLength );
-			var decoded_message = encoding.decode( encoded_message );
 
-			console.log( `${ws.id}: ${decoded_message}` );
+process.on( "SIGINT", () => {
 
-			if ( ! Array.isArray( decoded_message ) ) return;
+	console.log( "SIGINT received, stopping..." );
+	broadcast( null, [ 'say', null, 'Server shut down' ] );
+	process.exit();
 
-			var request = decoded_message.shift();
+} );
 
-			requests.hasOwnProperty( request ) && requests[ request ]( ws, ...decoded_message );
 
-		} );
+const httpserver = http.createServer( ( req, res ) => {
 
-		ws_connection[ ws.id ] = ws;
+	if ( "GET" === req.method ) {
 
-		console.log( `${ws.id} connected` );
+		var content = "";
 
-		send( ws, [ 'connected', ws.id ] );
+		try {
+
+			var filepath = "." + ( ( "/" === req.url ) ? "/index.html" : req.url );
+
+			content = fs.readFileSync( filepath );
+
+			res.writeHead( 200, {
+				"Content-Type": {
+					html: "text/html",
+					js: "text/javascript",
+					mjs: "text/javascript",
+					css: "text/css",
+					json: "application/json"
+				}[ filepath.split( "." ).pop().toLowerCase() ] || "text/*"
+			} );
+
+		} catch ( err ) {
+
+			res.code = "ENOENT" === err.code ? 404 : 500, res.writeHead( res.code, { "Content-Type": "text/html" } ), content = "<!doctype html><html><head><title>PBBG</title></head><body>" + err.message + "</body></html>";
+
+		}
+
+		return res.end( content, "utf-8" );
+
+	}
+
+} );
+
+
+httpserver.listen( process.env.PORT || 7714, () => {
+
+	console.log( `Server started on ${process.env.PORT ? 'port ' + process.env.PORT : 'http://localhost:7714'}` );
+
+} );
+
+wss = new WebSocket.Server( { server: httpserver } );
+
+wss.on( 'connection', ws => {
+
+	ws.id = 'Guest-' + Math.floor( ( 1 + Math.random() ) * 0x10000 ).toString( 16 ).substring( 1 );
+	ws.last_heard = ( new Date() ).getTime();
+
+	ws.on( 'close', () => {
+
+		if ( ws_connection.hasOwnProperty( ws.id ) ) delete ws_connection[ ws.id ];
+		if ( ws_identified.hasOwnProperty( ws.id ) ) delete ws_identified[ ws.id ];
+		ws_closed[ ws.id ] = ws;
 
 	} );
 
+	ws.on( 'pong', () => {
+
+		if ( ! ws_identified.hasOwnProperty( ws.id ) ) return;
+		ws_identified[ ws.id ].last_heard = ( new Date() ).getTime();
+
+	} );
+
+	ws.on( 'message', message => {
+
+		var encoded_message = message.buffer.slice( message.byteOffset, message.byteOffset + message.byteLength );
+		var decoded_message = encoding.decode( encoded_message );
+
+		console.log( `${ws.id}: ${decoded_message}` );
+
+		if ( ! Array.isArray( decoded_message ) ) return;
+
+		var request = decoded_message.shift();
+
+		requests.hasOwnProperty( request ) && requests[ request ]( ws, ...decoded_message );
+
+	} );
+
+	ws_connection[ ws.id ] = ws;
+
+	console.log( `${ws.id} connected` );
+
+	send( ws, [ 'connected', ws.id ] );
+
+} );
 
 
-	setInterval( () => {
 
-		var expired = ( new Date() ).getTime() - CLIENT_UPDATE_INTERVAL * 2;
+setInterval( () => {
 
-		for ( const id in ws_connection ) {
+	var expired = ( new Date() ).getTime() - CLIENT_UPDATE_INTERVAL * 2;
 
-			var ws = ws_connection[ id ];
+	for ( const id in ws_connection ) {
 
-			if ( ws.last_heard < expired ) {
+		var ws = ws_connection[ id ];
 
-				console.log( `${id} failed to identify` );
-				delete ws_connection[ id ];
-				ws.terminate();
+		if ( ws.last_heard < expired ) {
 
-			}
-
-		}
-
-		for ( const id in ws_identified ) {
-
-			var ws = ws_identified[ id ];
-
-			if ( ws.last_heard < expired ) {
-
-				console.log( `${ws.id} disconnected, ${id}` );
-				broadcast( ws, [ 'disconnected', ws.id ] );
-				delete ws_identified[ id ];
-				ws.terminate();
-
-			}
-
-			ws.ping();
-
-		}
-
-		for ( const id in ws_closed ) {
-
-			var ws = ws_closed[ id ];
-
-			if ( ws.last_heard < expired ) {
-
-				console.log( `${ws.id} closed` );
-				delete ws_closed[ id ];
-				ws.terminate();
-
-			}
+			console.log( `${id} failed to identify` );
+			delete ws_connection[ id ];
+			ws.terminate();
 
 		}
 
-	}, CLIENT_UPDATE_INTERVAL );
+	}
 
-}
+	for ( const id in ws_identified ) {
+
+		var ws = ws_identified[ id ];
+
+		if ( ws.last_heard < expired ) {
+
+			console.log( `${ws.id} disconnected, ${id}` );
+			broadcast( ws, [ 'disconnected', ws.id ] );
+			delete ws_identified[ id ];
+			ws.terminate();
+
+		}
+
+		ws.ping();
+
+	}
+
+	for ( const id in ws_closed ) {
+
+		var ws = ws_closed[ id ];
+
+		if ( ws.last_heard < expired ) {
+
+			console.log( `${ws.id} closed` );
+			delete ws_closed[ id ];
+			ws.terminate();
+
+		}
+
+	}
+
+}, CLIENT_UPDATE_INTERVAL );
+
