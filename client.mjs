0b70bd4c8c81bc578f00d6e92c60af32e37fdf56
sunladen <sunladenmail@gmail.com>
2021-06-08 17:33:43 +1000

    asdf

--- client.mjs
+++ client.mjs
@@ -1,39 +1,62 @@
 import Chat from './chat.mjs';
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set( 0, 0, 10 );
-camera.lookAt( 0, 0, 0 );
+class World {
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	constructor( container ) {
 
-window.addEventListener( 'resize', resize => {
+		this.scene = new THREE.Scene();
+		this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		this.camera.position.set( 0, 0, 10 );
+		this.camera.lookAt( 0, 0, 0 );
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
+		this.renderer = new THREE.WebGLRenderer();
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
 
-} );
+		this.container = container;
+
+		container.append( renderer.domElement );
+
+		this.fitContainer();
+
+		container.addEventListener( 'resize', this.fitContainer );
+
+		this.raycaster = new THREE.Raycaster();
+		this.mouse = new THREE.Vector2();
+
+		container.addEventListener( 'mousemove', event => {
+
+			this.mouse.x = ( event.clientX / this.renderer.width ) * 2 - 1;
+			this.mouse.y = - ( event.clientY / this.renderer.height ) * 2 + 1;
+
+		} );
+
+	}
+
+	fitContainer() {
+
+		var rect = this.container.getBoundingClientRect();
+		this.camera.aspect = rect.width / rect.height;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize( rect.width, rect.height );
+
+	}
+
+	render() {
+
+		this.renderer.render( this.scene, this.camera );
+
+	}
+
+}
+
+
+const world = new World( document.body );
 
 const geometry = new THREE.PlaneGeometry( 100, 100, 100, 100 );
 const material = new THREE.MeshBasicMaterial( { color: 0x224411, side: THREE.DoubleSide } );
 const plane = new THREE.Mesh( geometry, material );
 scene.add( plane );
-
-renderer.render( scene, camera );
-
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-
-document.addEventListener( 'mousemove', mousemove => {
-
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-} );
 
 
 var host = location.host.startsWith( 'localhost' ) ? location.host : 'scandalous-antique-mosquito.glitch.me';
@@ -42,5 +65,6 @@ chat.focus();
 
 function animate() {
 }
+
 animate();
 
