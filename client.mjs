d4d9f6817b7a085b4f6ec4bc38dec6d1dbc9d8f0
sunladen <sunladenmail@gmail.com>
2021-06-09 19:01:32 +1000

    asdf

--- client.mjs
+++ client.mjs
@@ -15,7 +15,7 @@ class World {
 
 		this.container = container;
 
-		container.append( renderer.domElement );
+		container.append( this.renderer.domElement );
 
 		this.fitContainer();
 
@@ -31,6 +31,8 @@ class World {
 
 		} );
 
+		this.render();
+
 	}
 
 	fitContainer() {
@@ -44,6 +46,8 @@ class World {
 
 	render() {
 
+		requestAnimationFrame( this.render.bind( this ) );
+
 		this.renderer.render( this.scene, this.camera );
 
 	}
@@ -51,20 +55,81 @@ class World {
 }
 
 
-const world = new World( document.body );
 
-const geometry = new THREE.PlaneGeometry( 100, 100, 100, 100 );
-const material = new THREE.MeshBasicMaterial( { color: 0x224411, side: THREE.DoubleSide } );
-const plane = new THREE.Mesh( geometry, material );
-scene.add( plane );
+class CanvasPlane {
+
+	/**
+	 * @param {Float} width
+	 * @param {Float} heigth
+	 * @param {Integer=1} widthSegments
+	 * @param {Integer=1} heightSegments
+	 */
+	constructor( width, height, widthSegments = 1, heightSegments = 1 ) {
+
+		this.ctx = document.createElement( 'canvas' ).getContext( '2d' );
+		this.ctx.canvas.width = width;
+		this.ctx.canvas.height = height;
+		this.ctx.fillStyle = '#FFF';
+		this.ctx.fillRect( 0, 0, this.ctx.canvas.width, this.ctx.canvas.height );
+		this.texture = new THREE.CanvasTexture( this.ctx.canvas );
+
+		this.geometry = new THREE.PlaneGeometry( width, height, widthSegments, heightSegments );
+		this.material = new THREE.MeshBasicMaterial( { map: this.texture } );
+		this.mesh = new THREE.Mesh( this.geometry, this.material );
+
+	}
+
+}
+
+
+class Ground extends CanvasPlane {
+
+	constructor( width, depth ) {
+
+		super( width, depth, width, depth );
+
+		this.vertices = this.geometry.attributes.position;
+
+		for ( var i = 0; i < this.vertices.count; i ++ ) {
+
+			var x = this.vertices.getX( i );
+			var y = this.vertices.getY( i );
+			var z = this.vertices.getZ( i );
+
+			y += - 1 + Math.random() * 2;
+
+			this.vertices.setXYZ( i, x, y, z );
+
+		}
+
+		this.geometry.verticesNeedUpdate = true;
+
+	}
+
+	update() {
+	}
+
+}
+
+class Player extends CanvasPlane {
+
+	 constructor() {
+
+		 super( 1, 1 );
+
+	}
+
+}
+
+
+const world = new World( document.body );
+const ground = new Ground( 100, 100 );
+
+world.scene.add( ground.mesh );
 
 
 var host = location.host.startsWith( 'localhost' ) ? location.host : 'scandalous-antique-mosquito.glitch.me';
 const chat = new Chat( `${location.protocol.replace( 'http', 'ws' )}//${host}`, document.body );
 chat.focus();
 
-function animate() {
-}
-
-animate();
 
