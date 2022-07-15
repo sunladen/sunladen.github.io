f1021db7fe229e7c3f1d1996d54ba00f0038a006
sunladen <sunladenmail@gmail.com>
2022-07-15 16:06:32 +1000

    verifyClient

--- pbbg-game-jam-2022/client.js
+++ pbbg-game-jam-2022/client.js
@@ -1,3 +1,9 @@
+const updateInterval = 2000;
 const serverURL = new URL( document.location.host === 'localhost:8000' ? 'ws://localhost:6500/' : 'wss://daffodil-polite-seat.glitch.me/' );
 const socket = new WebSocket( serverURL );
 socket.onopen = () => console.log( `Connected to ${serverURL}` );
+socket.onclose = () => setTimeout( () => window.location.replace( window.location.href ), updateInterval );
+socket.onmessage = ( e ) => {
+	const messages = JSON.parse( e.data );
+	console.log( 'Messages from server ', messages );
+}
--- pbbg-game-jam-2022/server.js
+++ pbbg-game-jam-2022/server.js
@@ -1,6 +1,8 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer( { port: process.env.PORT } );
+const port = process.env.PORT;
+const verifyClient = ( info ) => [ 'http://localhost:8000', 'https://sunladen.github.io' ].indexOf( info.req.headers.origin ) > - 1;
+const wss = new WebSocketServer( { port, verifyClient } );
 
 wss.on( 'connection', ( ws, req ) => {
 
